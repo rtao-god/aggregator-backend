b1a5d3f5e346496f701cb0047f6c01df3e00a46c
rtao-god <rtaogod@gmail.com>
2026-08-04 07:29:44 +0400

    Authorize Catalog owner implementation assemblies [skip ci]

---
+++ src/Catalog/Catalog.Domain/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Catalog.Application")]
+[assembly: InternalsVisibleTo("Catalog.Infrastructure")]
+[assembly: InternalsVisibleTo("Catalog.Domain.Tests")]
