2731a0da18eb05529f8cfce1ab05c85ea3ae30a7
rtao-god <rtaogod@gmail.com>
2026-08-04 12:41:43 +0400

    Define explicit Catalog worker composition [skip ci]

---
+++ src/Catalog/Catalog.Worker/CatalogWorkerServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.DependencyInjection;
+using Platform.Messaging;
+
+namespace Aggregator.Catalog.Worker;
+
+public static class CatalogWorkerServiceCollectionExtensions
+{
+    public static IServiceCollection AddCatalogWorker(
+        this IServiceCollection services,
+        CatalogWorkerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(options);
+        options.CreateOutboxDispatcherOptions().Validate();
+        options.CreateRabbitMqPublisherOptions().Validate();
+
+        services.AddSingleton(options);
+        services.AddSingleton(options.CreateRabbitMqPublisherOptions());
+        services.AddSingleton(options.CreateOutboxDispatcherOptions());
+        services.AddSingleton<RabbitMqEventPublisher>();
+        services.AddSingleton<IIntegrationEventPublisher>(provider =>
+            provider.GetRequiredService<RabbitMqEventPublisher>());
+        services.AddSingleton<PostgresOutboxDispatcher>();
+        services.AddHostedService<CatalogOutboxWorker>();
+        return services;
+    }
+}
