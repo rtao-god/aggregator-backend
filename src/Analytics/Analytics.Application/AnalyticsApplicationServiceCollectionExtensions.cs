fd9932e517e629b2cac8442f282eeea8201ce079
rtao-god <rtaogod@gmail.com>
2026-08-04 16:07:31 +0400

    Register canonical Analytics aggregate service [skip ci]

--- src/Analytics/Analytics.Application/AnalyticsApplicationServiceCollectionExtensions.cs
+++ src/Analytics/Analytics.Application/AnalyticsApplicationServiceCollectionExtensions.cs
@@ -10,6 +10,7 @@ public static class AnalyticsApplicationServiceCollectionExtensions
         services.AddSingleton(TimeProvider.System);
         services.AddScoped<SubmitInteractionEventService>();
         services.AddScoped<ReadDailyListingMetricsService>();
+        services.AddScoped<RebuildDailyAnalyticsMetricsService>();
         return services;
     }
 }
