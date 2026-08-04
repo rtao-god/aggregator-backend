79398fbcc72df6077e85f76978223c0cb6bf65b6
rtao-god <rtaogod@gmail.com>
2026-08-04 12:28:00 +0400

    Define Promotion overlay API policies [skip ci]

---
+++ src/Promotion/Promotion.Overlay.Api/PromotionOverlayApiContracts.cs
@@ -0,0 +1,11 @@
+namespace Aggregator.Promotion.Overlay.Api;
+
+public static class PromotionOverlayAuthorizationPolicies
+{
+    public const string Publish = "promotion.overlay.publish";
+}
+
+public static class PromotionOverlayRateLimitPolicies
+{
+    public const string Command = "promotion-overlay-command";
+}
