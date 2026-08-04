f19aa7192e7e2803d507bb15ac0dc95a78d55a80
rtao-god <rtaogod@gmail.com>
2026-08-04 13:36:37 +0400

    Compose Promotion owner worker [skip ci]

---
+++ src/Promotion/Promotion.Worker/Program.cs
@@ -0,0 +1,23 @@
+using Aggregator.Promotion.Application;
+using Aggregator.Promotion.Infrastructure;
+using Aggregator.Promotion.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Platform.Messaging;
+using Platform.Observability;
+
+var builder = Host.CreateApplicationBuilder(args);
+var options = PromotionWorkerOptions.FromConfiguration(builder.Configuration);
+builder.Services.AddSingleton(options);
+builder.Services.AddPromotionApplication();
+builder.Services.AddPromotionInfrastructure(builder.Configuration);
+builder.Services.AddSingleton(options.CreatePublisherOptions());
+builder.Services.AddSingleton(options.CreateOutboxOptions());
+builder.Services.AddSingleton<RabbitMqEventPublisher>();
+builder.Services.AddSingleton<IIntegrationEventPublisher>(services =>
+    services.GetRequiredService<RabbitMqEventPublisher>());
+builder.Services.AddSingleton<PostgresOutboxDispatcher>();
+builder.Services.AddHostedService<PromotionOwnerWorker>();
+builder.Services.AddPlatformObservability(builder.Configuration, "promotion-worker");
+
+await builder.Build().RunAsync();
