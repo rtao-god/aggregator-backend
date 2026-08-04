32ece88d07ee74afcb97e7837a66802723622a8b
rtao-god <rtaogod@gmail.com>
2026-08-04 07:26:46 +0400

    Register Catalog application services [skip ci]

---
+++ src/Catalog/Catalog.Application/CatalogApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aggregator.Catalog.Application;
+
+public static class CatalogApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddCatalogApplication(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        services.AddSingleton(TimeProvider.System);
+        services.AddScoped<CatalogConfigurationService>();
+        services.AddScoped<CatalogListingService>();
+        services.AddScoped<CatalogPublicationService>();
+        services.AddScoped<CatalogClaimService>();
+        return services;
+    }
+}
