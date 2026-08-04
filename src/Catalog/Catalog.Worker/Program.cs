19362d7f37fca662e5cc6313adb24c8883e2754c
rtao-god <rtaogod@gmail.com>
2026-08-04 12:42:03 +0400

    Use canonical Catalog worker composition [skip ci]

--- src/Catalog/Catalog.Worker/Program.cs
+++ src/Catalog/Catalog.Worker/Program.cs
@@ -1,19 +1,10 @@
 using Aggregator.Catalog.Worker;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Platform.Messaging;
 using Platform.Observability;
 
 var builder = Host.CreateApplicationBuilder(args);
 var options = CatalogWorkerOptions.FromConfiguration(builder.Configuration);
-builder.Services.AddSingleton(options);
-builder.Services.AddSingleton(options.CreateRabbitMqPublisherOptions());
-builder.Services.AddSingleton(options.CreateOutboxDispatcherOptions());
-builder.Services.AddSingleton<RabbitMqEventPublisher>();
-builder.Services.AddSingleton<IIntegrationEventPublisher>(services =>
-    services.GetRequiredService<RabbitMqEventPublisher>());
-builder.Services.AddSingleton<PostgresOutboxDispatcher>();
-builder.Services.AddHostedService<CatalogOutboxWorker>();
+builder.Services.AddCatalogWorker(options);
 builder.Services.AddPlatformObservability(builder.Configuration, "catalog-worker");
 
 await builder.Build().RunAsync();
