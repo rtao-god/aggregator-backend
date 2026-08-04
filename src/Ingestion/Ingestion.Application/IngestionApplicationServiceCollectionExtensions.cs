5c57cf780ae014a78624e351b6e9e9e813b31fe9
rtao-god <rtaogod@gmail.com>
2026-08-04 11:03:36 +0400

    Register Ingestion upload application services

--- src/Ingestion/Ingestion.Application/IngestionApplicationServiceCollectionExtensions.cs
+++ src/Ingestion/Ingestion.Application/IngestionApplicationServiceCollectionExtensions.cs
@@ -9,6 +9,8 @@ public static class IngestionApplicationServiceCollectionExtensions
         ArgumentNullException.ThrowIfNull(services);
         services.AddScoped<RegisterIngestionBatchService>();
         services.AddScoped<ReadIngestionBatchService>();
+        services.AddScoped<PrepareIngestionUploadService>();
+        services.AddScoped<CompleteIngestionUploadService>();
         return services;
     }
 }
