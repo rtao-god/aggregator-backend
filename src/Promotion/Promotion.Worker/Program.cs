a3cc168e87bf50af6fe44017267df7c5163c38df
rtao-god <rtaogod@gmail.com>
2026-08-10 12:31:57 +0400

    Config (Promotion worker): activate Analytics usage consumer

--- src/Promotion/Promotion.Worker/Program.cs
+++ src/Promotion/Promotion.Worker/Program.cs
@@ -10,10 +10,14 @@ var builder = Host.CreateApplicationBuilder(args);
 var options = PromotionWorkerOptions.FromConfiguration(builder.Configuration);
 var eligibilityOptions =
     PromotionEligibilityProjectionWorkerOptions.FromConfiguration(builder.Configuration);
+var usageOptions =
+    PromotionUsageProjectionWorkerOptions.FromConfiguration(builder.Configuration);
 builder.Services.AddSingleton(options);
 builder.Services.AddSingleton(eligibilityOptions);
+builder.Services.AddSingleton(usageOptions);
 builder.Services.AddPromotionApplication();
 builder.Services.AddPromotionInfrastructure(builder.Configuration);
+builder.Services.AddPromotionUsageProjectionInfrastructure();
 builder.Services.AddSingleton(options.CreatePublisherOptions());
 builder.Services.AddSingleton(options.CreateOutboxOptions());
 builder.Services.AddSingleton<RabbitMqEventPublisher>();
@@ -22,6 +26,7 @@ builder.Services.AddSingleton<IIntegrationEventPublisher>(services =>
 builder.Services.AddSingleton<PostgresOutboxDispatcher>();
 builder.Services.AddHostedService<PromotionOwnerWorker>();
 builder.Services.AddHostedService<PromotionEligibilityProjectionWorker>();
+builder.Services.AddHostedService<PromotionUsageProjectionWorker>();
 builder.Services.AddPlatformObservability(builder.Configuration, "promotion-worker");
 
 await builder.Build().RunAsync();
